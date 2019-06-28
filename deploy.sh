86b7e461180b150ceb1fa2b63c7cc8664f47a91b
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-06-28 16:02:42 -0400

    deploy.sh: add debugging options

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,17 @@
 ## - https://github.com/manubot/rootstock/blob/ddb0288895cd5bc5dab117fb366c52216a717d0e/ci/deploy.sh
 ## - https://github.com/wp-cli/wp-cli/issues/3798
 
+# Set options for extra caution & debugging
+set -o errexit \
+    -o nounset \
+    -o pipefail
+
+# Ensure command traces are disabled while dealing with the private key
+set +o xtrace
+echo -e $GITHUB_DEPLOY_PRIVATE_KEY > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+set -o xtrace
+
 # Configure git
 git config --global push.default simple
 git config --global user.name "Travis CI"
@@ -16,11 +27,6 @@ git remote set-url origin "git@github.com:$TRAVIS_REPO_SLUG.git"
 # Travis does a shallow and single branch git clone
 git remote set-branches --add origin gh-pages
 git fetch origin gh-pages:gh-pages
-
-# Ensure command traces are disabled while dealing with the private key
-set +o xtrace
-echo -e $GITHUB_DEPLOY_PRIVATE_KEY > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
 
 commit_message="\
 Generate catalog output on $(date --iso --utc)
